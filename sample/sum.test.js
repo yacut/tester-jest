14171ab4faf8c2402de522b0c69f6f1d8a6e5fac
yacut <yacut@protonmail.com>
2017-03-05 20:02:28 +0100

    fix parse

--- sample/sum.test.js
+++ sample/sum.test.js
@@ -1,9 +1,13 @@
 const sum = require('./sum');
+const assert = require('assert');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe('mocha runner', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 
-test('adds 1 + 2  not to equal 4', () => {
-  expect(sum(1, 2)).toBe(4);
+  it('should return console log text and shows strings diff', () => {
+    console.info('test#string');
+    assert.equal('stringA', 'stringB');
+  });
 });
