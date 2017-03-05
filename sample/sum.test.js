f98c84ac0a64a1c0cb11cedc0407d0a166012883
yacut <yacut@protonmail.com>
2017-03-05 23:07:36 +0100

    make it works

--- sample/sum.test.js
+++ sample/sum.test.js
@@ -1,13 +1,10 @@
 const sum = require('./sum');
 const assert = require('assert');
 
-describe('mocha runner', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
+test('adds 1 + 2 to equal 3?', () => {
+  expect(sum(1, 2)).toBe(3);
+});
 
-  it('should return console log text and shows strings diff', () => {
-    console.info('test#string');
-    assert.equal('stringA', 'stringB');
-  });
+test('should return console log text and shows strings diff', () => {
+  assert.equal('stringA', 'stringB');
 });
